3ffcbb7356b1652a7620b218dabb111c4f172fce
Francesco Cassese <francescocassese9@gmail.com>
2026-03-24 15:52:32 +0100

    Aggiunta soluzione FizzBuzz + Versione con l'uso dei ternari concatenati

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//===================================================================
+//DEVO STAMPARE I NUMERI DA 1 A 100 E "SEGNALARE" I MULTIPLI DI 3 E 5
+//===================================================================
+
+//SE i numeri sono divisibili per entrambi stampa FIZZBUZZ
+
+//ALTRIMENTI SE il numero è un mutiplo di 3 stampa FIZZ
+
+//ALTRIMENTI SE il numero è un multiplo di 5 stampa BUZZ
+
+//ALTRIMENTI stampa il numero normale
+
+
+//LOGICA CICLO
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`FIZZBUZZ`);
+    } else if (i % 3 === 0) {
+        console.log(`FIZZ`);
+    } else if (i % 5 === 0) {
+        console.log(`BUZZ`);
+    } else {
+        console.log(i);
+    }
+}
+
+//VERSIONE CON TERNARI CONCATENATI
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(
+//         i % 3 === 0 && i % 5 === 0 ? `FIZZBUZZ` :
+//         i % 3 === 0 ? `FIZZ` :
+//         i % 5 === 0 ? `BUZZ` :
+//         i
+//     );
+// }
